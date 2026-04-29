9c9a50168904ba0584ac9d73d4aa8f9e13222c2e
Crapbin Hyderabad <cbh.alkapur@gmail.com>
2026-04-29 16:13:05 +0530

    new numbers problems

--- Number-systems/Numbers.java
+++ Number-systems/Numbers.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class Numbers {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        //call isPrimeOrNot
+        // isPrimeOrNot();
+        // productAndSum(234);
+        countSetBits(24);
+    }
+
+    static void isPrimeOrNot() {
+                Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
-        int number = sc.nextInt();
+        int num = sc.nextInt();
 
         boolean isPrime = true;
 
-        if (number < 1) {
+        if (num < 1) {
             isPrime = false;
         } else {
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
+            for (int i = 2; i < Math.sqrt(num); i++) {
+                if (num % i == 0) {
                     isPrime = false;
                     break;
                 }
@@ -21,10 +28,52 @@ public class Numbers {
         }
 
         if (isPrime) {
-            System.out.println("Number is prime number");
+            System.out.println("Number is a prime number");
         } else {
             System.out.println("Number is not a prime number");
         }
     }
 
+    static int productAndSum(int n) {
+        // find the difference between product and sum of its digits
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int remainder = n % 10;
+
+            product *= remainder;
+            sum += remainder;
+            n /= 10;
+        }
+
+        int result = product - sum;
+
+        System.out.println(result);
+
+        return result;
+    }
+
+    static int countSetBits(int n) {
+        // count the number of set bits in a number
+        int counter = 0;
+
+        while (n != 0) {
+            n = n & (n - 1);
+            counter++;
+        }
+
+        System.out.println(counter);
+
+        return counter;
+    }
+
+    
+
+
 }
+
+
+
+// Difference between product and sum of a number
+
